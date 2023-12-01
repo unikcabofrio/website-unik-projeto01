1dca808a0bdf99fbe399ddf924078ccde3e9ec71
unikcabofrio <contato.unikcabofrio@gmail.com>
2023-11-30 22:18:55 -0300

    '2218'

--- src/components/header.js
+++ src/components/header.js
@@ -16,12 +16,12 @@ export default function Header() {
             <div className="nav-wrapper">
               <a href={'/'} className="brand-logo">LOGO</a>
               <ul>
-                <li><a onClick={()=>{scrolldiv("home")}}>Home</a></li>
-                <li><a onClick={()=>{scrolldiv("service")}}>Serviços</a></li>
-                <li><a onClick={()=>{scrolldiv("photos")}}>Nossa Estrutura</a></li>
-                <li><a onClick={()=>{scrolldiv("infor")}}>Informação</a></li>
-                <li><a onClick={()=>{scrolldiv("price")}}>Preço</a></li>
-                <li><a onClick={()=>{scrolldiv("footer")}}>Contato</a></li>
+                <li><a onClick={()=>{scrolldiv("home")}}><i className="fi fi-sr-house-chimney"></i>Home</a></li>
+                <li><a onClick={()=>{scrolldiv("service")}}><i className="fi fi-sr-heart"></i>Serviços</a></li>
+                <li><a onClick={()=>{scrolldiv("photos")}}><i className="fi fi-sr-building"></i>Nossa Estrutura</a></li>
+                <li><a onClick={()=>{scrolldiv("infor")}}><i className="fi fi-sr-comment-info"></i>Informação</a></li>
+                <li><a onClick={()=>{scrolldiv("price")}}><i className="fi fi-sr-badge-dollar"></i>Preço</a></li>
+                <li><a onClick={()=>{scrolldiv("footer")}}><i className="fi fi-sr-phone-call"></i>Contato</a></li>
               </ul>
             </div>
           </nav>
