49f691eabcbce9ec2f8d7b370e60009b6460f6d6
unikcabofrio <contato.unikcabofrio@gmail.com>
2023-12-04 01:51:40 -0300

    '0151'

--- src/components/Pages/Contat.js
+++ src/components/Pages/Contat.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 import Image from "next/image"
 import Link from "next/link"
 
-import {ButtonSmall} from "@/utils/Button"
+import {ButtonSmall} from "@/utils/button"
 
 import Logo from '@/assets/logo_black-AE0xqKEXpLtVQXnM.png'
 
